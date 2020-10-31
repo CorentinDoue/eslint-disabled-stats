77727281abe5ab4a69775d7c01faae71d2bd0126
CorentinDoue <doue.coco@hotmail.fr>
2020-10-31 12:59:24 +0100

    feat: get files

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { printTitle } from './print';
+import { getFileContent, getFilesPaths } from './getFiles';
 
 export const computeEslintDisabledStats = async (options: Options) => {
+  const { pattern } = options;
   printTitle('Eslint Disabled Stats');
-  console.log(options);
+  const filePaths = await getFilesPaths(pattern);
+  await Promise.all(
+    filePaths.map(async (filePath) => {
+      const fileContent = await getFileContent(filePath);
+    }),
+  );
 };
