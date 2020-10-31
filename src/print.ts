fa305a2f57b7c69ab636ade22538dace5fc95051
CorentinDoue <doue.coco@hotmail.fr>
2020-10-31 14:09:41 +0100

    chore: improve print

--- src/print.ts
+++ src/print.ts
@@ -1,18 +1,23 @@
-import { red, yellow, blue, green } from 'chalk';
+import { red, yellow, blue, green, dim } from 'chalk';
 import { textSync } from 'figlet';
+import { Unicode } from './unicode';
 
 export const printTitle = (title: string): void =>
   console.log(blue(textSync(title)));
 
 export const printError = (error: Error): void => {
-  console.error(red(`ERROR: ${error.message}`));
+  console.error(red(`${Unicode.RedCross} ${error.message}`));
   process.exitCode = 1;
 };
 
 export const printWarning = (message: string): void => {
-  console.warn(yellow(`WARNING: ${message}`));
+  console.warn(yellow(`${Unicode.Warning} ${message}`));
 };
 
 export const printSuccess = (message: string): void => {
-  console.info(green(`SUCCESS: ${message}`));
+  console.info(green(`${Unicode.CheckMark} ${message}`));
+};
+
+export const printInfo = (message: string): void => {
+  console.info(dim(`${Unicode.Information} ${message}`));
 };
---
+++ src/unicode.ts
@@ -0,0 +1,7 @@
+export enum Unicode {
+  ListDot = '\u2022',
+  RedCross = '\u274C',
+  Warning = '\u26A0',
+  CheckMark = '\u2714',
+  Information = '\u2139',
+}
